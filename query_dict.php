59f80bdb3551e530b91a3adf91506085b58d9564
Jen Liu <jkliu@mit.edu>
2013-06-20 23:07:11 +0800

    separate queries for character positioning. changed to fluid 2-col layout

--- query_dict.php
+++ query_dict.php
@@ -13,75 +13,94 @@
 	ChromePhp::log($term);
 	$dbh->query("SET NAMES utf8");
 
-
-// **using prepare statements and variables
-// 	$sql = "SELECT *
-// 	 			FROM  `entry`
-// 	 			WHERE  `traditional` LIKE  '%$term%'
-// 	 			OR `simplified` LIKE '%$term%'
-// 	 			OR `pinyin` LIKE '$term'";
-
-// 	ChromePhp::log('Hello？');
-// try {
-// 	$stmt = $dbh->prepare($sql);
-// 	$params = array("%$term%", "%$term%", "$term");
-// 	$stmt->execute($params);
-// 	ChromePhp::log('again');
-
-// 	$result = $stmt->$fetchAll();
-// 	print_r($result);
-// } catch(PDOException $e){
-//     echo $this->pack('dbError', $e->getMessage());
-//     ChromePhp::log($this->pack('dbError', $e->getMessage()));
-// }
-
 	$sql = "SELECT *
 				FROM `entry`
 				WHERE `traditional` = '$term'
 				OR `simplified` = '$term'";
 
-	$exact = $dbh->query($sql)->fetch();
+	$exact = $dbh->query($sql);
 
 	// using direct query
-	// consider doing separate queries for positioning, each ordered by length
-	$sql = "SELECT $chartype, pinyin, english, 1 as rank from entry
+	$sql = "SELECT $chartype, pinyin, english from entry
 				WHERE $chartype LIKE '$term%'
-			UNION
-			SELECT $chartype, pinyin, english, 2 as rank from entry
-				WHERE $chartype LIKE '_$term%'
-			UNION
-			SELECT $chartype, pinyin, english, 3 as rank from entry
-				WHERE $chartype LIKE '__$term%'";
+				ORDER BY LENGTH($chartype) ASC";
 
+	$first_result = $dbh->query($sql);
+
+	$sql = "SELECT $chartype, pinyin, english from entry
+				WHERE $chartype LIKE '_%$term%'
+				ORDER BY LENGTH($chartype) ASC";
 
 	$contains_result = $dbh->query($sql);
 
-
 ?>
+
+
+
 <?php if($exact) : ?>
 	<div class="well">
-		<div class="primary result">
-			<?php echo $exact[0]; ?>
-		</div>
-		<br /><?php echo pinyin_addaccents($exact['pinyin']); ?>
-		<br /><?php echo $exact['english']; ?>
+			<div class="primary result">
+				<div class="row">
+					<div class="span2">
+						<span class="char main"><?php echo $term; ?></span>
+					</div>
+					<div class="span9">
+						<?php foreach(($exact) as $row) : ?>
+							<span class="pinyin"><?php echo pinyin_addaccents($row['pinyin']); ?></span>
+							- <span class="english"><?php echo $row['english']; ?></span><br/>
+						<?php endforeach ?>
+					</div>
+				</div>
+			</div>
 	</div>
 <?php endif ?>
 
 
-<?php if($contains_result) : ?>
-	<table class="table">
-		<?php foreach(($contains_result) as $row) : ?>
-			<tr>
-				<td>
-					<?php echo highlight($row[0], $term); ?>
-					<br/>
-					<span class="pinyin">
-						<?php echo pinyin_addaccents($row['pinyin']); ?>
-					</span>
-				</td>
-				<td><?php echo $row['english']; ?></td>
-			</tr>
-		<?php endforeach ?>
-	</table>
-<?php endif ?>
+<ul class="nav nav-tabs">
+	<li><a href="#first-char" data-toggle="tab">first</a></li>
+	<li><a href="#contains-char" data-toggle="tab">contains</a></li>
+	<li><a href="#all" data-toggle="tab">all</a></li>
+</ul>
+
+<div class="tab-content">
+	<div class="tab-pane active" id="first-char">
+		<?php if($first_result) : ?>
+			<table class="terms">
+				<?php foreach(($first_result) as $row) : ?>
+					<tr>
+						<td>
+							<span class="char"><?php echo highlight($row[0], $term); ?></span>
+						</td>
+						<td>
+							<span class="pinyin">
+								<?php echo pinyin_addaccents($row['pinyin']); ?>
+							</span>
+						</td>
+						<td><?php echo $row['english']; ?></td>
+					</tr>
+				<?php endforeach ?>
+			</table>
+		<?php endif ?>
+	</div>
+	<div class="tab-pane" id="contains-char">
+		<?php if($contains_result) : ?>
+			<table class="terms table">
+				<?php foreach(($contains_result) as $row) : ?>
+					<tr>
+						<td>
+							<span class="char"><?php echo highlight($row[0], $term); ?></span>
+						</td>
+						<td>
+							<span class="pinyin">
+								<?php echo pinyin_addaccents($row['pinyin']); ?>
+							</span>
+						</td>
+						<td><?php echo $row['english']; ?></td>
+					</tr>
+				<?php endforeach ?>
+			</table>
+		<?php endif ?>
+	</div>
+	<div class="tab-pane" id="all">
+	</div>
+</div>
